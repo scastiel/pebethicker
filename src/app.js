24ab95152db4b7ce51a415f1c7a9480247e6f004
Sébastien <sebastien@macseb.home>
2016-03-10 11:41:09 +0100

    Clean up and refactoring

--- src/app.js
+++ src/app.js
@@ -1,73 +1,80 @@
-/**
- * Welcome to Pebble.js!
- *
- * This is where you write your app.
- */
-
 var UI = require('ui');
 var Settings = require('settings');
 var ajax = require('ajax');
 
+var PRESS_SELECT_TO_REFRESH_TEXT = 'Press select to refresh';
+var ERROR_OCCURRED_TEXT = 'Error occurred';
+var LOADING_TEXT = 'Loading...';
+var SETTINGS_URL = 'http://scastiel.me/pebethicker/settings/';
+var GATECOIN_API_URL = 'https://www.gatecoin.com/api/Public/LiveTickers';
+
 var items = [];
 var section = {
-  items: items
+    items: items
 };
 var menu = new UI.Menu({
-  sections: [section]
+    sections: [section]
 });
-menu.on('select', refresh);
+menu.on('select', refreshTickersFromAPI);
 menu.show();
 
-refresh();
+refreshTickersFromAPI();
 
 var tickersToShow;
 
-function refresh() {
-  tickersToShow = Settings.option('tickers');
-  if (!tickersToShow || tickersToShow.length === 0) {
-    tickersToShow = ['ETHBTC', 'ETHEUR'];
-    Settings.option('tickers', tickersToShow);
-  }
-  
-  section.title = 'Loading...';
-  menu.section(0, section);
-  ajax({ url: 'https://www.gatecoin.com/api/Public/LiveTickers', type: 'json' },
-      function(data, status, request) {
-        var tickers = {};
-        data.tickers.forEach(function(ticker) {
-          tickers[ticker.currencyPair] = ticker;
-        });
-        section.title = 'Press select to refresh';
-        items.length = 0;
-        tickersToShow.forEach(function(tickerToShow) {
-          var ticker = tickers[tickerToShow];
-          var last;
-          if (ticker) {
+function updateTickers(tickers) {
+    section.title = PRESS_SELECT_TO_REFRESH_TEXT;
+    items.length = 0;
+    tickersToShow.forEach(function (tickerToShow) {
+        var ticker = tickers[tickerToShow];
+        var last;
+        if (ticker) {
             last = ticker.last;
-          } else if (tickerToShow === 'ETHUSD') {
+        } else if (tickerToShow === 'ETHUSD') {
             last = tickers.ETHBTC.last * tickers.BTCUSD.last;
-          }
-          
-          if (last) {
+        }
+
+        if (last) {
             var fromCurrency = tickerToShow.slice(0, 3);
             var toCurrency = tickerToShow.slice(3);
-            var formattedLast = Math.round(last*10000)/10000;
+            var formattedLast = Math.round(last * 10000) / 10000;
             var string = '1 ' + fromCurrency + ' = ' + formattedLast + ' ' + toCurrency;
             items.push({ title: tickerToShow, subtitle: string });
-          }
+        }
+    });
+    menu.section(0, section);
+}
+
+function fetchTickersToShowFromSettings() {
+    tickersToShow = Settings.option('tickers');
+    if (!tickersToShow || tickersToShow.length === 0) {
+        tickersToShow = ['ETHBTC', 'ETHEUR'];
+        Settings.option('tickers', tickersToShow);
+    }
+}
+
+function refreshTickersFromAPI() {
+    fetchTickersToShowFromSettings();
+    section.title = LOADING_TEXT;
+    menu.section(0, section);
+    ajax({ url: GATECOIN_API_URL, type: 'json' },
+        function (data) {
+            var tickers = {};
+            data.tickers.forEach(function (ticker) {
+                tickers[ticker.currencyPair] = ticker;
+            });
+            updateTickers(tickers);
+        },
+        function (error) {
+            console.error(error);
+            section.title = ERROR_OCCURRED_TEXT;
+            menu.section(0, section);
         });
-        menu.section(0, section);
-      },
-      function(error, status, request) {
-        section.title = 'Error occurred';
-        menu.section(0, section);
-      });
 }
 
 Settings.config(
-  { url: 'http://scastiel.me/pebethicker/settings/' },
-  function(e) {
-    console.log("Options", e.options);
-    tickersToShow = e.options.tickers;
-  }
+    { url: SETTINGS_URL },
+    function (e) {
+        tickersToShow = e.options.tickers;
+    }
 );
